dcc65de0cf258a364962af6e87ead2c9422c1c37
HanningtoneM <hanningtonepompyy@gmail.com>
2022-05-09 19:51:37 +0300

    add javascript function show

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 // mobile menu
 
+const openMenu = document.querySelector('.open-menu');
+const mobileMenu = document.querySelector('.desktopmenu');
+const closeMenu = document.querySelector('.close-menu');
+
+function show() {
+  mobileMenu.style.display = 'flex';
+  mobileMenu.style.top = '0';
+  document.querySelector('.main').classList.add('blur');
+  document.querySelector('.logo').classList.add('blur');
+  openMenu.classList.add('blur');
+}
