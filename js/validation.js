5b5b1ce6db5bccf0c6c67d2972c66373383ccc72
HanningtoneM <hanningtonepompyy@gmail.com>
2022-05-11 19:18:12 +0300

    Add data in validation.js

--- js/validation.js
+++ js/validation.js
@@ -16,20 +16,8 @@ const validateEmail = (value) => {
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-const form = document.querySelector('.contact-form');
-const message = 'Email address must be in lowercase';
 
-const showMessage = (message) => {
-  const msg = document.querySelector('.form-submission-error');
-  msg.innerHTML = `<p class="error">${message}</p>`;
-};
-
-const validateEmail = (value) => {
-  const emailText = value.toLowerCase();
-  if (emailText.localeCompare(value) === 0) {
-    return true;
-  }
-const messsageText = document.querySelector('.form-submission-error');
+  const messsageText = document.querySelector('.form-submission-error');
   const emailInput = form.elements[1];
   const email = validateEmail(emailInput.value);
   if (email) {
